cce68faf38a52ea4fcd9bd6c99d73b14ad9c835c
dowlsss <megamobarak@gmail.com>
2022-05-08 13:49:34 +0500

    0.0.1

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ const tg = Telegram.WebApp;
 const initData = tg.initData || '';
 const initDataUnsafe = tg.initDataUnsafe || {};
 const userData = tg.WebAppUser || {};
-console.log(userData)
 
 document.querySelector('#card--name').innerHTML = `Hi, ${initDataUnsafe.user.first_name}!`
